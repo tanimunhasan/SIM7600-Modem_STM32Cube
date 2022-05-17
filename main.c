ba1076c0094983eb1677a51738f367b9602d5431
Tanimun Hasan <59294968+tanimunhasan@users.noreply.github.com>
2022-05-17 17:33:03 +0600

    Create main.c

---
+++ main.c
@@ -0,0 +1,35 @@
+/* USER CODE BEGIN Includes */
+#include <string.h>
+#include <stdio.h>
+/* USER CODE END Includes */
+
+  /* USER CODE BEGIN 2 */
+  char mobileNumber[] = "+61498749497";  // Enter the Mobile Number you want to send to
+  char ATcommand[80];
+  uint8_t buffer[30] = {0};
+  uint8_t ATisOK = 0;
+  while(!ATisOK){
+  		sprintf(ATcommand,"AT\r\n");
+  		HAL_UART_Transmit(&huart1,(uint8_t *)ATcommand,strlen(ATcommand),1000);
+  		HAL_UART_Receive (&huart1, buffer, 30, 100);
+  		HAL_Delay(1000);
+  		if(strstr((char *)buffer,"OK")){
+  			ATisOK = 1;
+  		}
+  		HAL_Delay(1000);
+  		memset(buffer,0,sizeof(buffer));
+  }
+  sprintf(ATcommand,"AT+CMGF=1\r\n");
+  HAL_UART_Transmit(&huart1,(uint8_t *)ATcommand,strlen(ATcommand),1000);
+  HAL_UART_Receive (&huart1, buffer, 30, 100);
+  HAL_Delay(1000);
+  memset(buffer,0,sizeof(buffer));
+  sprintf(ATcommand,"AT+CMGS=\"%s\"\r\n",mobileNumber);
+  HAL_UART_Transmit(&huart1,(uint8_t *)ATcommand,strlen(ATcommand),1000);
+  HAL_Delay(100);
+  sprintf(ATcommand,"Hello World, STM32 started%c",0x1a);
+  HAL_UART_Transmit(&huart1,(uint8_t *)ATcommand,strlen(ATcommand),1000);
+  HAL_UART_Receive (&huart1, buffer, 30, 100);
+  memset(buffer,0,sizeof(buffer));
+  HAL_Delay(4000);
+  /* USER CODE END 2 */
